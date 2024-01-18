ec9e58a3b134b4a8e2800d0a8ce87ccd0f145374
datanggo <zhenli-ren@foxmail.com>
2024-01-18 17:14:17 +0800

    新增

---
+++ vue-admin-template-master/src/api/user.js
@@ -0,0 +1,28 @@
+// 在引入二次封装后的axios
+import request from '@/utils/request'
+
+// 对外暴露登录的接口函数
+export function login(data) {
+  return request({
+    url: '/vue-admin-template/user/login',
+    method: 'post',
+    data
+  })
+}
+
+// 对外暴露获取用户信息的函数
+export function getInfo(token) {
+  return request({
+    url: '/vue-admin-template/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+// 对外暴露退出登录的函数
+export function logout() {
+  return request({
+    url: '/vue-admin-template/user/logout',
+    method: 'post'
+  })
+}
